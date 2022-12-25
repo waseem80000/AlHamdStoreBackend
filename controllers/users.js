164b96e15774dc75c29755aea3c07ca49e6c742a
waseem80000 <mw7391346@gmail.com>
2022-12-25 14:25:24 +0500

    token expiry removed

--- controllers/users.js
+++ controllers/users.js
@@ -17,7 +17,7 @@ export const signin = async (req, res) => {
 
     if (!isPasswordCorrect) return res.status(400).json({ message: 'Invalid credentials' });
 
-    const token = jwt.sign({ email: oldUser.email, id: oldUser._id }, secret, { expiresIn: '1h' });
+    const token = jwt.sign({ email: oldUser.email, id: oldUser._id }, secret);
     res.status(200).json({ result: oldUser, token });
   } catch (err) {
     res.status(500).json({ message: 'Something went wrong' });
